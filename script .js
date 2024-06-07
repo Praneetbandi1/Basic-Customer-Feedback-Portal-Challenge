71e13ce39d6c91f365d4c33d40178d8e281b1f48
Praneetbandi1 <praneetbandi@usf.edu>
2024-06-07 18:08:20 -0400

    Update script .js

--- script .js
+++ script .js
@@ -1,1 +1,20 @@
+//U4636100
+function submitCustomerFeedback(event){
+event. preventDefault();
+const name=document.getElementById('customerName').ariaValueMax;
+    const rating =document.getElementById('customerName').ariaValueMax;
+    const comments=document.getElementById('customerComments').ariaValueMax;
 
+    if(!name|| !rating || !comments) {
+    alert('Please fill out all the fields.');
+    }
+    if(name!="" && comments!="") {
+
+        const customerFeedbackHTML='<p>Customer Name:' + name + '<br>Customer Rating: '
+        + rating + '<br>Customer Comments: ' + comments +'<p>';
+        document.getElementById('customer-feedback-display').innerHTML+=customerFeedbackHTML;
+
+
+    }
+
+}
