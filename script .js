a1f4b90b679a4a713361b3fbcf361a927174b07e
Praneetbandi1 <praneetbandi@usf.edu>
2024-06-08 01:14:30 -0400

    Update script .js

--- script .js
+++ script .js
@@ -1,9 +1,9 @@
 //U4636100
 function submitCustomerFeedback(event){
 event. preventDefault();
-const name=document.getElementById('customerName').ariaValueMax;
-    const rating =document.getElementById('customerName').ariaValueMax;
-    const comments=document.getElementById('customerComments').ariaValueMax;
+const name=document.getElementById('customerName').value;
+    const rating =document.getElementById('customerRating').value;
+    const comments=document.getElementById('customerComments').value;
 
     if(!name|| !rating || !comments) {
     alert('Please fill out all the fields.');
